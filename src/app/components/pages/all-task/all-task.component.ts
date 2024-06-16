2aeee1a5edff8f8e2f9c7f47e8f1d10d07b118b6
Vijayakumar-Developer <vijai1993@gmail.com>
2024-06-16 09:49:33 +0530

    todo-angular

---
+++ src/app/components/pages/all-task/all-task.component.ts
@@ -0,0 +1,49 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpService } from '../../../services/http.service';
+import { PageTitleComponent } from '../../page-title/page-title.component';
+import { TaskListComponent } from '../../task-list/task-list.component';
+
+
+@Component({
+  selector: 'app-all-task',
+  standalone: true,
+  imports: [FormsModule,PageTitleComponent,TaskListComponent],
+  templateUrl: './all-task.component.html',
+  styleUrl: './all-task.component.scss'
+})
+export class AllTaskComponent {
+
+  newTask="";
+  taskList:any[]=[];
+  httpService=inject(HttpService);
+  
+  ngOnInit(){
+    this.getAllTasks();
+  }
+  addTask(){
+    console.log("addTask",this.newTask);
+    this.httpService.addTask(this.newTask).subscribe(()=>{
+      this.newTask="";
+      this.getAllTasks();
+    })
+  }
+  getAllTasks(){
+    this.httpService.getAllTasks().subscribe((result:any)=>{
+      this.taskList=result;
+    })
+  }
+
+  onComplete(task:any){
+    task.completed=true;
+  this.httpService.updateTask(task).subscribe(
+    ()=>{}
+  )
+  }
+  onImportant(task:any){
+    task.important=true;
+    this.httpService.updateTask(task).subscribe(
+      ()=>{}
+    )
+  }
+}
---
+++ src/app/services/http.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable,inject } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  
+  httpClient=inject(HttpClient);
+  constructor() { }
+
+  addTask(task:string){
+    return this.httpClient.post("http://localhost:3000/tasks",{
+      title:task
+    })
+
+  }
+  getAllTasks(){
+   return this.httpClient.get("http://localhost:3000/tasks");
+  }
+  updateTask(task:any){
+    return this.httpClient.get("http://localhost:3000/tasks/"+task.id,task);
+  }
+}
